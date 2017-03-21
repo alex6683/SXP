16b9864c36bd368c1653f631f42c2a415b11c0c3
alex6683 <alex6683@hotmail.fr>
2017-03-21 15:16:40 +0100

    Create package BlockChain and class BlockChainEstablisher

---
+++ src/main/java/protocol/impl/BlockChainEstablisher.java
@@ -0,0 +1,13 @@
+package protocol.impl;
+
+import protocol.api.Establisher;
+
+/**
+ * Created by alex on 21/03/17.
+ */
+public class BlockChainEstablisher extends Establisher<?,?,?,?> {
+
+    public BlockChainEstablisher() {
+
+    }
+}
