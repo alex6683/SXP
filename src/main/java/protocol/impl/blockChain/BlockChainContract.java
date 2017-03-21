2b36295eb0a295f765ca24c48d49038894995a9f
alex6683 <alex6683@hotmail.fr>
2017-03-21 15:23:11 +0100

    Create Class BCcontract

---
+++ src/main/java/protocol/impl/blockChain/BlockChainContract.java
@@ -0,0 +1,13 @@
+package protocol.impl.blockChain;
+
+import protocol.api.EstablisherContract;
+
+/**
+ * Created by alex on 21/03/17.
+ */
+public class BlockChainContract extends EstablisherContract<?,?,?,?> {
+
+    public BlockChainContract() {
+
+    }
+}
