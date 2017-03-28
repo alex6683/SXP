fc0268da0f9c7f7cfa2b2c35a47d7ddda6eddc4c
alex6683 <alex6683@hotmail.fr>
2017-03-28 14:33:01 +0200

    Libs Ethereum add with Gradle

--- src/main/java/protocol/impl/blockChain/EthereumImpl.java
+++ src/main/java/protocol/impl/blockChain/EthereumImpl.java
@@ -1,11 +1,9 @@
 package protocol.impl.blockChain;
 
 import org.ethereum.facade.Ethereum;
-import org.ethereum.facade.EthereumFactory;
-
 /**
  * Created by alex on 21/03/17.
  */
 public class EthereumImpl {
-    Ethereum eth = EthereumFactory() ;
+    Ethereum eth ;
 }
