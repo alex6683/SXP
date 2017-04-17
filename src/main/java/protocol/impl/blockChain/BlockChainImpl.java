1b7d270969e681229e3c9259b80f6d09b6e29a81
alex6683 <alex6683@hotmail.fr>
2017-04-17 23:32:09 +0200

    Gradle Ok

---
+++ src/main/java/protocol/impl/blockChain/BlockChainImpl.java
@@ -0,0 +1,15 @@
+package protocol.impl.blockChain;
+
+import model.entity.ContractEntity;
+import model.entity.User;
+import org.ethereum.facade.Ethereum;
+
+/**
+ * Created by alex on 14/04/17.
+ */
+public class BlockChainImpl {
+    private Ethereum ethereum ;
+    private User user ;
+    private ContractEntity contract ;
+
+}
--- src/main/java/protocol/impl/blockChain/EthereumImpl.java
+++
@@ -1,9 +0,0 @@
-package protocol.impl.blockChain;
-
-import org.ethereum.facade.Ethereum;
-/**
- * Created by alex on 21/03/17.
- */
-public class EthereumImpl {
-    Ethereum eth ;
-}
