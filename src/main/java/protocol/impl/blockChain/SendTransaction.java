46b4b4ba499cca1fadcb28baa318759d5cf84c6f
alex6683 <alex6683@hotmail.fr>
2017-04-24 22:03:29 +0200

    build

--- src/main/java/protocol/impl/blockChain/SendTransaction.java
+++ src/main/java/protocol/impl/blockChain/SendTransaction.java
@@ -3,14 +3,24 @@ package protocol.impl.blockChain;
 import org.ethereum.core.Transaction;
 import org.ethereum.core.TransactionReceipt;
 import org.ethereum.crypto.ECKey;
+import org.ethereum.db.ByteArrayWrapper;
+import org.ethereum.facade.Ethereum;
 import org.ethereum.util.ByteUtil;
 
 import java.math.BigInteger;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by alex on 24/04/17.
  */
 public abstract class SendTransaction {
+
+    private Ethereum ethereum ;
+    private Map<ByteArrayWrapper, TransactionReceipt> txWaiters =
+            Collections.synchronizedMap(new HashMap<ByteArrayWrapper, TransactionReceipt>());
+
     public TransactionReceipt sendTxAndWait(ECKey senderAddress, byte[] receiveAddress, byte[] data) throws Exception {
         BigInteger nonce = ethereum.getRepository().getNonce(senderAddress.getAddress());
         Transaction tx = new Transaction(
@@ -27,4 +37,26 @@ public abstract class SendTransaction {
 
         return waitForTx(tx.getHash());
     }
+
+    public TransactionReceipt waitForTx(byte[] txHash) throws Exception {
+        ByteArrayWrapper txHashW = new ByteArrayWrapper(txHash);
+        txWaiters.put(txHashW, null);
+        long startBlock = ethereum.getBlockchain().getBestBlock().getNumber();
+        while(true) {
+            TransactionReceipt receipt = txWaiters.get(txHashW);
+            if (receipt != null) {
+                return receipt;
+            } else {
+                long curBlock = ethereum.getBlockchain().getBestBlock().getNumber();
+                if (curBlock > startBlock + 16) {
+                    throw new RuntimeException("The transaction was not included during last 16 blocks: " + txHashW.toString().substring(0,8));
+                } else {
+                    System.out.println("Waiting for block with transaction 0x" + txHashW.toString().substring(0,8) +
+                            " included (" + (curBlock - startBlock) + " blocks received so far) ...");
+                }
+            }
+            synchronized (ethereum.getBlockchain().getBestBlock()) {
+                ethereum.getBlockchain().getBestBlock().wait(20000);
+            }
+        } }
 }
